193a1d99349726dd6d3449b366dc5333c7c7c9e9
Jesus <jdavid10hoyos@gmail.com>
2026-03-05 11:23:37 -0500

    Update

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://invoicemaker-k61o.vercel.app/api',
+    baseURL: 'https://invoicemaker-k61o.vercel.app',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
